da93ad9d9bcf7e93ab0a8c67239fdf6cc9d7c1b6
fschwiet <fschwiet@gmail.com>
2018-12-13 11:31:58 -0300

    using a timer so we can see what happens to the lifetime of an embedded control that isn't always present

--- src/index.js
+++ src/index.js
@@ -5,18 +5,38 @@ import Latitude from './latitude'
 import ComponentLifecycleLogger from './component-lifecycle-logger'
 
 
-function App() {
+class App extends React.Component {
 
-    return (
-        <div>
-            <DivWithMessage message="hello, world."/>
+    state = { time: 0}
 
-            <ComponentLifecycleLogger message="every component has its lifecycle events"/>
-            
-            <Latitude/>
-        </div>
+    componentDidMount() {
+        setInterval(() => this.setState({time: this.state.time + 1}), 1000);
+    }
 
-    );
+    render() {
+
+        var controlToTestUnmount = null;
+        
+        if (this.state.time % 5 !== 0)
+        {
+            controlToTestUnmount = (
+                <ComponentLifecycleLogger message={"current time is " + this.state.time} />
+            );
+        }
+
+
+        return (
+            <div>
+                <DivWithMessage message="hello, world."/>
+    
+                <Latitude/>
+
+                {controlToTestUnmount}
+
+            </div>
+    
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
